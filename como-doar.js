53fc657944873b2f7585649d2c7feae6c944aae0
Miguel Castilho <miguel.arrudacastilho@gmail.com>
2026-09-21 15:36:37 -0300

    tela finalizada

--- como-doar.js
+++ como-doar.js
@@ -1,41 +1,23 @@
-// Seleciona todos os botões que servem para ABRIR as respostas
 var botoesAbrir = document.querySelectorAll(".btnMenu");
-
-// Seleciona todos os botões que servem para FECHAR as respostas
 var botoesFechar = document.querySelectorAll(".menuClose");
-
-
 // Percorre todos os botões de abrir
 botoesAbrir.forEach(function (botaoAbrir) {
-
     // Adiciona um evento de clique em cada botão
     botaoAbrir.addEventListener("click", function () {
-
         // Pega o elemento .container que contém o botão clicado
         var pergunta = botaoAbrir.parentElement;
-
         // Adiciona a classe "isOpen" no container
         // Essa classe faz a resposta aparecer através do CSS
         pergunta.classList.add("isOpen");
-
     });
-
 });
-
-
-// Percorre todos os botões de fechar
 botoesFechar.forEach(function (botaoFechar) {
-
     // Adiciona um evento de clique em cada botão
     botaoFechar.addEventListener("click", function () {
-
         // Pega o elemento .container que contém o botão clicado
         var pergunta = botaoFechar.parentElement;
-
         // Remove a classe "isOpen" do container
         // Assim, a resposta volta a ficar escondida
         pergunta.classList.remove("isOpen");
-
     });
-
 });
